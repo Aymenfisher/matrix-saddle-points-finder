1e63e57bfbf15c9728b54ad2f072dcf02313dbf7
Aymenfisher <fisheraymen@gmail.com>
2018-04-02 01:07:28 +0100

    2nd version , can deal with all cases

--- matriice.py
+++ matriice.py
@@ -1,20 +1,30 @@
-matrice=[[1,2,3],[0,5,3],[-1,-2,-1],[50,-55,3]]
-for n in matrice:
-	line_number=matrice.index(n)
-	maximum=max(n)
-	index_of_max=n.index(maximum)
-	for i in matrice:
-		minimum=i[index_of_max]
-		if maximum==minimum:
-			pass
-		elif maximum>minimum:
-			break
-	else:
-		print 'point col found!!! :'
-		print "ligne: ",line_number+1
-		print "colonne: ",index_of_max+1
-		print "Valeur: ",maximum
-
-
-			
-			
+matrice=[[1,2,3],[0,5,3],[-1,-2,-1],[-1,-2,-1]]
+number_of_lines=len(matrice)
+number_of_columns=len(matrice[0])
+line=0
+while line<number_of_lines:
+	maximum=max(matrice[line])
+	column=0
+	while column<number_of_columns:
+		suspect=matrice[line][column]
+		if suspect==maximum:
+			line2=0
+			while line2<number_of_lines:
+				to_check=matrice[line2][column]
+				if suspect==to_check :
+				  line2+=1
+				  pass
+				elif suspect>to_check:
+					line2+=1
+					break	
+				if line2==number_of_lines-1:
+					print "point-col found :"
+					print "ligne :",line+1
+					print "colonne: ",column+1
+					print "Valeur: ",maximum
+					print '-'*6
+					line2+=1
+				line2+=1	
+					
+		column+=1
+	line+=1	
